dcaadc0b940326592a91a131629a29123fd4eb13
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-11-19 21:09:59 +0200

    slice-len-cap.go translated

--- _content/tour/moretypes/slice-len-cap.go
+++ _content/tour/moretypes/slice-len-cap.go
@@ -8,15 +8,15 @@ func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
 	printSlice(s)
 
-	// Slice the slice to give it zero length.
+	// Здійснити зріз зрізу, щоб встановити йому нульову довжину.
 	s = s[:0]
 	printSlice(s)
 
-	// Extend its length.
+	// Збільшити його довжину
 	s = s[:4]
 	printSlice(s)
 
-	// Drop its first two values.
+	// Відкинути перші два значення.
 	s = s[2:]
 	printSlice(s)
 }
