819d8db33cd4d2efa3c7a26705fae550746903d9
Oleksandr Krutko <46520164+arsenalzp@users.noreply.github.com>
2022-12-08 01:58:47 +0200

    Update _content/tour/methods/exercise-reader.go
    Згоден
    
    Co-authored-by: radikh <radikhs@gmail.com>

--- _content/tour/methods/exercise-reader.go
+++ _content/tour/methods/exercise-reader.go
@@ -6,7 +6,7 @@ import "golang.org/x/tour/reader"
 
 type MyReader struct{}
 
-// TODO: Створіть функцію-метод Read([]byte) (int, error) для структурного типу MyReader.
+// TODO: Створіть метод Read([]byte) (int, error) для структурного типу MyReader.
 
 func main() {
 	reader.Validate(MyReader{})
