a9630d07bd746dd05056a17e66adf69327caa748
Rostyslav Pylypiv <97484707+rostis232@users.noreply.github.com>
2023-04-07 22:52:30 +0300

    moretypes: translation is finished (#5)
    Translated concurrency
    
    Co-authored-by: Eugene Chernyshov <chernyshov.eugene@gmail.com>
    Co-authored-by: illia <illiadimura@gmail.com>
    Co-authored-by: Eugene Chernyshov <eugene@thefloorcyber.com>
    Co-authored-by: Aleksandr Krutko <alexander.krutko@gmail.com>
    Co-authored-by: Oleksandr Krutko <46520164+arsenalzp@users.noreply.github.com>
    Co-authored-by: Roman Kosyi <mantilocal@gmail.com>
    Co-authored-by: illiafox <61962654+illiafox@users.noreply.github.com>
    Co-authored-by: radikh <radikhs@gmail.com>

--- _content/tour/basics/short-variable-declarations.go
+++ _content/tour/basics/short-variable-declarations.go
@@ -1,3 +1,4 @@
+//go:build OMIT
 // +build OMIT
 
 package main
@@ -7,7 +8,7 @@ import "fmt"
 func main() {
 	var i, j int = 1, 2
 	k := 3
-	c, python, java := true, false, "no!"
+	c, python, java := true, false, "ні!"
 
 	fmt.Println(i, j, k, c, python, java)
 }
